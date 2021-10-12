c54c79afa5b46d1acfcc18fc593750818744551e
xizhou <1150293155@qq.com>
2021-10-12 12:49:03 +0800

    1

---
+++ src/test/java/tut4/TestCalc.java
@@ -0,0 +1,10 @@
+package tut4;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCalc {
+    @Test
+    public void testadd(){
+        Assertions.assertEquals(2, Calc.add(1,1));
+    }
