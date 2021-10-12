e3853f55a9b44b8aac918be9f4e2b1805964e381
xizhou <1150293155@qq.com>
2021-10-12 13:53:06 +0800

    travis

--- src/main/java/tut4/Calc.java
+++ src/main/java/tut4/Calc.java
@@ -4,5 +4,5 @@ public class Calc{
     public static int add(int num1, int num2) {
         return num1+num2;
     }
-    public static int substract(int num1, int num2) { return num1+num2; }
+    public static int substract(int num1, int num2) { return num1-num2; }
     }
